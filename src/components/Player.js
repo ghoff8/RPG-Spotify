5f1005fb60ceaa97901083364de9a16c6d07e682
Grant Hoffmann <grant.hoffmann@leidos.com>
2019-09-04 11:17:24 -0400

    update

--- src/components/Player.js
+++ src/components/Player.js
@@ -49,7 +49,7 @@ function Player(props) {
                 method: 'GET',
                 url: 'http://localhost:3001/player'
             }).then(res => {
-                console.log(res.data)
+                console.log("Data" + res.data)
                 setPlayer(res.data)
             }).catch(err => {
                 console.log(err)
